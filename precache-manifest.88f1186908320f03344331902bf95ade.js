a59e25a0a8505be4c4f730219a0c05a260a7e819
pft\\Ruby_Feng <Ruby_Feng@PerfectCorp.com>
2020-09-04 17:27:19 +0800

    Updates

---
+++ precache-manifest.88f1186908320f03344331902bf95ade.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a059ff86d5e3a6c28ef54188227b14f1",
+    "url": "/tomato_clock/index.html"
+  },
+  {
+    "revision": "ac97a8b2bf8a261c5a9e",
+    "url": "/tomato_clock/static/css/main.086f7f2a.chunk.css"
+  },
+  {
+    "revision": "4403672296794b479f3b",
+    "url": "/tomato_clock/static/js/2.e818d6b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tomato_clock/static/js/2.e818d6b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac97a8b2bf8a261c5a9e",
+    "url": "/tomato_clock/static/js/main.a468ccff.chunk.js"
+  },
+  {
+    "revision": "19b4d47474a81b784947",
+    "url": "/tomato_clock/static/js/runtime-main.f3904079.js"
+  },
+  {
+    "revision": "e96b4269d4c32caf1f74ebd2806c5e73",
+    "url": "/tomato_clock/static/media/icon-analysis.e96b4269.svg"
+  },
+  {
+    "revision": "526aed07ca647772b1c7e37fce521ba1",
+    "url": "/tomato_clock/static/media/icon-bell.526aed07.svg"
+  },
+  {
+    "revision": "872654be6f7049208508b4b9dbb29ad9",
+    "url": "/tomato_clock/static/media/icon-cancel.872654be.svg"
+  },
+  {
+    "revision": "c53166308d41b6e9605d02735f8a4fc6",
+    "url": "/tomato_clock/static/media/icon-list.c5316630.svg"
+  },
+  {
+    "revision": "5094d45d055ee409d5375f38d8da5014",
+    "url": "/tomato_clock/static/media/icon-pause--orange.5094d45d.svg"
+  },
+  {
+    "revision": "a5c4a07e8f2bc1df8463be26262dea23",
+    "url": "/tomato_clock/static/media/icon-play--green.a5c4a07e.svg"
+  },
+  {
+    "revision": "03ecd12353c261056c6d161d7125352a",
+    "url": "/tomato_clock/static/media/music.03ecd123.mp3"
+  },
+  {
+    "revision": "4298a104d2031aa18070af31dbd4728f",
+    "url": "/tomato_clock/static/media/tomato--green.4298a104.svg"
+  },
+  {
+    "revision": "f4d252253f2234eeed22a10c0daf96fb",
+    "url": "/tomato_clock/static/media/tomato--orange.f4d25225.svg"
+  }
+]);
